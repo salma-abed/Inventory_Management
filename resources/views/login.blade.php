d3785621cbe482f422fc7e71f83d317c84e71972
Yosr Gamal <yosr.elqott@gmail.com>
2022-03-30 12:23:02 +0200

    added font-awesome link

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -16,9 +16,10 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" type="text/css" href="/css/app.css">
-    <!-- <link rel="stylesheet" type="text/css" href="/css/themify-icons.css"> -->
-    <!--<link rel="stylesheet" type="text/css" href="/css/font-awesome.min.css">-->
     <link rel="stylesheet" type="text/css" href="/css/helper.css">
 
 
