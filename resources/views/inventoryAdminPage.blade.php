5ec155afd40b678d4d73cbf79103456f2eb4b23a
OmegaElrefaie <omegaelrefaie@gmail.com>
2022-04-01 00:23:50 +0200

    editing button and input fields

--- resources/views/inventoryAdminPage.blade.php
+++ resources/views/inventoryAdminPage.blade.php
@@ -65,6 +65,7 @@
                 </div>
                 <div class="modal-body">
                 <form id="formElement" method="POST" action="">
+                        @csrf
                         <div class="mb-3">
                             <label class="form-label">Branch name</label>
                             <input type="text" class="form-control" id="Warehouse_name" name="Warehouse_name">
@@ -89,26 +90,23 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Add Printinghouse Branch</h5>
+                    <h5 class="modal-title" id="staticBackdropLabel">Add Printing_house Branch</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <form id="formElement">
+                <form id="formElement" method="POST" action="">
+                         @csrf
                         <div class="mb-3">
-                            <label class="form-label">Code</label>
-                            <input type="text" class="form-control" id="quantity">
+                            <label class="form-label">Printing_house name</label>
+                            <input type="text" class="form-control" id="Printing_house_name" name="Printing_house_name">
                         </div>
                         <div class="mb-3">
-                            <label class="form-label">Branch name</label>
-                            <input type="text" class="form-control" id="productname">
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label">Location</label>
-                            <input type="text" class="form-control" id="location">
+                            <label class="form-label">Printing_house_address</label>
+                            <input type="text" class="form-control" id="Printing_house_address" name="Printing_house_address">
                         </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Submit</button>
+                    <button type="submit" class="btn btn-primary" data-bs-dismiss="modal">Submit</button>
                     </form>
                 </div>
             </div>
@@ -126,22 +124,19 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <form id="formElement">
-                        <div class="mb-3">
-                            <label class="form-label">Code</label>
-                            <input type="text" class="form-control" id="quantity">
-                        </div>
+                <form id="formElement" method="POST" action="">
+                         @csrf
                         <div class="mb-3">
                             <label class="form-label">Branch name</label>
-                            <input type="text" class="form-control" id="productname">
+                            <input type="text" class="form-control" id="Store_name" name="Store_name">
                         </div>
                         <div class="mb-3">
-                            <label class="form-label">Location</label>
-                            <input type="text" class="form-control" id="location">
+                            <label class="form-label">Store Location</label>
+                            <input type="text" class="form-control" id="Store_address" name="Store_address">
                         </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Submit</button>
+                    <button type="submit" class="btn btn-primary" data-bs-dismiss="modal">Submit</button>
                     </form>
                 </div>
             </div>
