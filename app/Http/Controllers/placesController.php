ee01f2c158a81aa466244c8edbe8fefe2ca37192
OmegaElrefaie <omegaelrefaie@gmail.com>
2022-04-01 00:25:12 +0200

    adding store() to insert places data into DB

--- app/Http/Controllers/placesController.php
+++ app/Http/Controllers/placesController.php
@@ -35,7 +35,47 @@ class placesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $Object = new places();
+        $Object->place_name = strip_tags($request->input('Warehouse_name'));
+        
+  
+        if(isset($Object->place_name))
+        {
+            $request->validate([
+                'Warehouse_name'=> 'required',
+                'Warehouse_address'=>'required',
+
+            ]);
+
+            //POST
+            
+
+            $Object->place_type = "warehouse";
+            $Object->place_name = strip_tags($request->input('Warehouse_name'));
+            $Object->place_address = strip_tags($request->input('Warehouse_address'));
+            $Object->save();
+
+        }
+        // $Object->place_name = strip_tags($request->input('Printing_house_name'));
+
+        // if(isset($Object->place_name))
+        // {
+        //     $request->validate([
+        //         'Printing_house_name'=> 'required',
+        //         'Printing_house_address'=>'required',
+
+        //     ]);
+
+        //     //POST
+            
+        //     $Object->place_type = "printinghouse";
+        //     $Object->place_name = strip_tags($request->input('Printing_house_name'));
+        //     $Object->place_address = strip_tags($request->input('Printing_house_address'));
+        //     $Object->save();
+
+        // }
+            return back(); //basically refreshes after data is sent.
+
     }
 
     /**
