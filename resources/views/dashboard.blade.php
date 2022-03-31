2d6ef08548916d74187863c4b955e6eb10b6f2cf
OmegaElrefaie <omegaelrefaie@gmail.com>
2022-03-31 20:25:23 +0200

    added a graph

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -105,8 +105,60 @@
                     </div>
 
                    
+<svg height="342" version="1.1" width="599.328" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="overflow: hidden; position: relative; top: -0.1875px;">
+    <desc style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">Created with Raphaël 2.2.0</desc>
+    <defs style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></defs>
+    <text x="32.84765625" y="303" text-anchor="end" font-family="sans-serif" font-size="12px" stroke="none" fill="#888888" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: end; font-family: sans-serif; font-size: 12px; font-weight: normal;" font-weight="normal">
+        <tspan dy="4" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">0</tspan></text>
+        <path fill="none" stroke="#eef0f2" d="M45.34765625,303H574.328" stroke-width="0.5" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></path>
+        <text x="32.84765625" y="233.5" text-anchor="end" font-family="sans-serif" font-size="12px" stroke="none" fill="#888888" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: end; font-family: sans-serif; font-size: 12px; font-weight: normal;" font-weight="normal">
+            <tspan dy="4" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">25</tspan></text>
+            <path fill="none" stroke="#eef0f2" d="M45.34765625,233.5H574.328" stroke-width="0.5" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></path>
+            <text x="32.84765625" y="164" text-anchor="end" font-family="sans-serif" font-size="12px" stroke="none" fill="#888888" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: end; font-family: sans-serif; font-size: 12px; font-weight: normal;" font-weight="normal">
+                <tspan dy="4" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">50</tspan></text>
+                <path fill="none" stroke="#eef0f2" d="M45.34765625,164H574.328" stroke-width="0.5" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></path>
+                <text x="32.84765625" y="94.50000000000003" text-anchor="end" font-family="sans-serif" font-size="12px" stroke="none" fill="#888888" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: end; font-family: sans-serif; font-size: 12px; font-weight: normal;" font-weight="normal"><tspan dy="4.000000000000028" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">75</tspan>
+            </text><path fill="none" stroke="#eef0f2" d="M45.34765625,94.50000000000003H574.328" stroke-width="0.5" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></path>
+            <text x="32.84765625" y="25" text-anchor="end" font-family="sans-serif" font-size="12px" stroke="none" fill="#888888" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: end; font-family: sans-serif; font-size: 12px; font-weight: normal;" font-weight="normal">
+                <tspan dy="4" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">100</tspan>
+        </text><path fill="none" stroke="#eef0f2" d="M45.34765625,25H574.328" stroke-width="0.5" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);"></path><text x="536.5436897321429" y="315.5" text-anchor="middle" font-family="sans-serif" font-size="12px" stroke="none" fill="#888888" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: middle; font-family: sans-serif; font-size: 12px; font-weight: normal;" font-weight="normal" transform="matrix(1,0,0,1,0,7)">
+            <tspan dy="4" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">2012</tspan></text>
+        <text x="385.40644866071426" y="315.5" text-anchor="middle" font-family="sans-serif" font-size="12px" stroke="none" fill="#888888" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: middle; font-family: sans-serif; font-size: 12px; font-weight: normal;" font-weight="normal" transform="matrix(1,0,0,1,0,7)"><tspan dy="4" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">2010</tspan></text>
+        <text x="234.2692075892857" y="315.5" text-anchor="middle" font-family="sans-serif" font-size="12px" stroke="none" fill="#888888" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: middle; font-family: sans-serif; font-size: 12px; font-weight: normal;" font-weight="normal" transform="matrix(1,0,0,1,0,7)">
+            <tspan dy="4" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">2008</tspan></text>
+        <text x="83.13196651785714" y="315.5" text-anchor="middle" font-family="sans-serif" font-size="12px" stroke="none" fill="#888888" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); text-anchor: middle; font-family: sans-serif; font-size: 12px; font-weight: normal;" font-weight="normal" transform="matrix(1,0,0,1,0,7)">
+            <tspan dy="4" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0);">2006</tspan></text>
+        <rect x="54.793733816964284" y="25" width="26.83823270089286" height="278" rx="0" ry="0" fill="#343957" stroke="none" fill-opacity="1" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); fill-opacity: 1;"></rect><rect x="84.63196651785714" y="52.80000000000001" width="26.83823270089286" height="250.2" rx="0" ry="0" fill="#5873fe" stroke="none" fill-opacity="1" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); fill-opacity: 1;"></rect><rect x="130.36235435267858" y="94.50000000000003" width="26.83823270089286" height="208.49999999999997" rx="0" ry="0" fill="#343957" stroke="none" fill-opacity="1" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); fill-opacity: 1;"></rect><rect x="160.20058705357144" y="122.30000000000001" width="26.83823270089286" height="180.7" rx="0" ry="0" fill="#5873fe" stroke="none" fill-opacity="1" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); fill-opacity: 1;"></rect><rect x="205.93097488839285" y="164" width="26.83823270089286" height="139" rx="0" ry="0" fill="#343957" stroke="none" fill-opacity="1" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); fill-opacity: 1;"></rect>
+        <rect x="235.7692075892857" y="191.8" width="26.83823270089286" height="111.19999999999999" rx="0" ry="0" fill="#5873fe" stroke="none" fill-opacity="1" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); fill-opacity: 1;"></rect><rect x="281.49959542410716" y="94.50000000000003" width="26.83823270089286" height="208.49999999999997" rx="0" ry="0" fill="#343957" stroke="none" fill-opacity="1" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); fill-opacity: 1;"></rect><rect x="311.337828125" y="122.30000000000001" width="26.83823270089286" height="180.7" rx="0" ry="0" fill="#5873fe" stroke="none" fill-opacity="1" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); fill-opacity: 1;"></rect>
+        <rect x="357.06821595982143" y="164" width="26.83823270089286" height="139" rx="0" ry="0" fill="#343957" stroke="none" fill-opacity="1" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); fill-opacity: 1;"></rect>
+        <rect x="386.9064486607143" y="191.8" width="26.83823270089286" height="111.19999999999999" rx="0" ry="0" fill="#5873fe" stroke="none" fill-opacity="1" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); fill-opacity: 1;"></rect><rect x="432.6368364955357" y="94.50000000000003" width="26.83823270089286" height="208.49999999999997" rx="0" ry="0" fill="#343957" stroke="none" fill-opacity="1" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); fill-opacity: 1;"></rect>
+        <rect x="462.47506919642854" y="122.30000000000001" width="26.83823270089286" height="180.7" rx="0" ry="0" fill="#5873fe" stroke="none" fill-opacity="1" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); fill-opacity: 1;"></rect>
+        <rect x="508.20545703125003" y="25" width="26.83823270089286" height="278" rx="0" ry="0" fill="#343957" stroke="none" fill-opacity="1" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); fill-opacity: 1;"></rect><rect x="538.0436897321429" y="52.80000000000001" width="26.83823270089286" height="250.2" rx="0" ry="0" fill="#5873fe" stroke="none" fill-opacity="1" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); fill-opacity: 1;"></rect></svg>
 
 
+    <div class="max-W-6xl mx-auto sm:px-6 lg:px-8">
+        <form class="form bg-white p-6 border-1" method="POST" action="">
+                @csrf
+                <div>
+                    <label class="text-sm" for="guitar-name">Guitar Name</label>
+                    <input class="text-lg border-1" type="text" id="guitar-name" name="guitar-name">
+                </div>
+                
+                <div>
+                    <label class="text-sm" for="brand">Brand</label>
+                    <input class="text-lg border-1" type="text" id="brand" name="brand">
+                </div>
+
+                <div
+                    «label class="text-sm" for="year">Year Made</label>
+                    <input class="text-lg border-1" type="text" id="year" name="year">
+                </div>
+
+                <div>
+                    <button class=" border-1" type="submit">Submit</button>
+                </div>
+        </forms>
+    </div>
 
 
                     <div class="row">
