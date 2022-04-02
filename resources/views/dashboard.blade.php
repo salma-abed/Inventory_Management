31e981322975f5552b3bb4f62e489fa4f812fddb
OmegaElrefaie <omegaelrefaie@gmail.com>
2022-04-02 04:03:26 +0200

    deleting unnecessary form

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -220,29 +220,7 @@
                     </svg>
 
 
-                    <div class="max-W-6xl mx-auto sm:px-6 lg:px-8">
-                        <form class="form bg-white p-6 border-1" method="POST" action="">
-                            @csrf
-                            <div>
-                                <label class="text-sm" for="guitar-name">Guitar Name</label>
-                                <input class="text-lg border-1" type="text" id="guitar-name" name="guitar-name">
-                            </div>
-
-                            <div>
-                                <label class="text-sm" for="brand">Brand</label>
-                                <input class="text-lg border-1" type="text" id="brand" name="brand">
-                            </div>
-
-                            <div «label class="text-sm" for="year">Year Made</label>
-                                <input class="text-lg border-1" type="text" id="year" name="year">
-                            </div>
-
-                            <div>
-                                <button class=" border-1" type="submit">Submit</button>
-                            </div>
-                            </forms>
-                    </div>
-
+                  
 
                     <div class="row">
                         <div class="col-lg-12">
