69d1129eaee874b964c17a16accb358b457049fa
Yosr Gamal <yosr.elqott@gmail.com>
2022-04-02 20:52:40 +0200

    removed view route of places

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,6 @@ Route::get('/', function () {
 Route::get('/employeetable',[employeeController::class,"view"])->name('view');
 
 
-Route::get('/places', function () {
-    return view('admin/placesAdminPage');
-});
 Route::get('/warehouse', function () {
     return view('warehouseManager/warehouseTable');
 });
@@ -34,4 +31,3 @@ Route::get('/dashboard',function(){
 Route::post('/inventory',[productController::class,'store']);
 Route::post('/places',[placesController::class,'store']);
 Route::get('/places',[placesController::class,'index']);
-
