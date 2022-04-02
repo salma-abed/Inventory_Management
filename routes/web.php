38106bf06e8b09a31eb5acd6708cd4042ffd8a37
OmegaElrefaie <omegaelrefaie@gmail.com>
2022-04-02 04:25:53 +0200

    changing route into proper page

--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,4 @@ Route::get('/dashboard',function(){
 });
 
 Route::post('/inventory',[productController::class,'store']);
-Route::post('/inventory',[placesController::class,'store']);
+Route::post('/places',[placesController::class,'store']);
