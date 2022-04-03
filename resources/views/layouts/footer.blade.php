a009e0f4280107664b016dbed9d327bb10befe0d
Yosr Gamal <yosr.elqott@gmail.com>
2022-04-03 16:59:22 +0200

    changed footer position

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,14 +1,9 @@
-<body class="d-flex flex-column min-vh-100" id="footerc">
-    <footer class="text-center text-white">
-        <!-- Grid container -->
-        <div class="container p-4"></div>
-        <!-- Grid container -->
+<!-- Grid container -->
+<div class="container-fluid"></div>
+<!-- Grid container -->
 
-        <!-- Copyright -->
-        <div class="text-center p-3" id="footer">
-            © 2022 Copyright:
-            <a class="text-white" href="https://2oolameme.com/">2oolamame.com</a>
-        </div>
-        <!-- Copyright -->
-    </footer>
-</body>
+<!-- Copyright -->
+<div class="text-center p-3" id="footer">
+    © 2022 Copyright:
+    <a class="text-white" href="https://2oolameme.com/">2oolamame.com</a>
+</div>
