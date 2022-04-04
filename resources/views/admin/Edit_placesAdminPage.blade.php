a3054294b2061d90e691ef156dfa50a80adb92e5
Yosr Gamal <yosr.elqott@gmail.com>
2022-04-04 16:48:37 +0200

    added css and bootstrap to the page

--- resources/views/admin/Edit_placesAdminPage.blade.php
+++ resources/views/admin/Edit_placesAdminPage.blade.php
@@ -1,53 +1,50 @@
+@extends('layouts.default')
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 
 <body>
-    
-    <div class="container">
+    @section('content')
 
-        <div class="modal-body">
-            <form id="formElement" method="POST" action="/update/{{$data[0]->place_id}}">
-                @csrf
-                <div class="mb-3">
-                    <label class="form-label">Place Name</label>
-                    <input type="text" class="form-control" id="place_name" value="{{$data[0]->place_name}}"
-                        name="place_name">
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+                <div class=" login-form">
+                    <h3>Edit</h3>
+                    <form id="formElement" method="POST" action="/update/{{$data[0]->place_id}}">
+                        @csrf
+                        <div class="mb-3">
+                            <label class="form-label">Place Name</label>
+                            <input type="text" class="form-control" id="place_name" value="{{$data[0]->place_name}}"
+                                name="place_name">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Location</label>
+                            <input type="text" class="form-control" id="place_location"
+                                value="{{$data[0]->place_address}}" name="place_location">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Product</label>
+                            <input type="text" class="form-control" id="product_name" value="{{$data[0]->product}}"
+                                name="product_name">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Quantity</label>
+                            <input type="text" class="form-control" id="quantity" value="{{$data[0]->quantity}}"
+                                name="quantity">
+                        </div>
+                        <label class="form-label">facility </label>
+                        <select id="type_of_place" name="type_of_place">
+                            <option value="Warehouse">Warehouse</option>
+                            <option value="Printhouse">Printhouse</option>
+                            <option value="Store">Store</option>
+                        </select>
+                        <button type="submit" class="btn btn-primary" data-bs-dismiss="modal">Update</button>
                 </div>
-                <div class="mb-3">
-                    <label class="form-label">Location</label>
-                    <input type="text" class="form-control" id="place_location" value="{{$data[0]->place_address}}"
-                        name="place_location">
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Product</label>
-                    <input type="text" class="form-control" id="product_name" value="{{$data[0]->product}}"
-                        name="product_name">
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Quantity</label>
-                    <input type="text" class="form-control" id="quantity" value="{{$data[0]->quantity}}"
-                        name="quantity">
-                </div>
-                <label class="form-label">facility </label>
-                <select id="type_of_place" name="type_of_place">
-                    <option value="Warehouse">Warehouse</option>
-                    <option value="Printhouse">Printhouse</option>
-                    <option value="Store">Store</option>
-                </select>
-        </div>
-        <div class="modal-footer">
-            <button type="submit" class="btn btn-primary" data-bs-dismiss="modal">update</button>
-            </form>
+                </form>
 
+            </div>
         </div>
     </div>
+    @endsection
 </body>
 
 </html>
