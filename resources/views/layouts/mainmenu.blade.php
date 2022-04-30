5579c7d2ec6b27547906485425b0321621b46a6c
Yosr Gamal <yosr.elqott@gmail.com>
2022-04-30 21:56:43 +0200

    added mainmenu

---
+++ resources/views/layouts/mainmenu.blade.php
@@ -0,0 +1,20 @@
+<body class="main-bg">
+    <div class="main-wrapper">
+        <div class="sidenav" id="mySideBar">
+            <div class="l">
+                <a href="index.html">
+                    <img src="{{ asset('images/logo.png') }}" width="50px" alt="">
+                </a>
+            </div>
+
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <a href="#">Dashboard</a>
+            <a href="#">My Profile</a>
+            <a href="#">Products</a>
+            <a href="#">Places</a>
+            <a href="#">Users</a>
+            <a href="#">History</a>
+        </div>
+    </div>
+
+</body>
