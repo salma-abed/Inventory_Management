8a954370fe58616e8663f104de55755067aa8d50
Yosr Gamal <yosr.elqott@gmail.com>
2022-05-03 20:35:07 +0200

    made mainmenu dynamic

--- resources/views/layouts/mainmenu.blade.php
+++ resources/views/layouts/mainmenu.blade.php
@@ -1,19 +1,18 @@
 <body class="main-bg">
     <div class="main-wrapper">
         <div class="sidenav" id="mySideBar">
-            <div class="l">
+            <div class="l d-flex justify-content-center">
                 <a href="index.html">
-                    <img src="{{ asset('images/logo.png') }}" width="50px" alt="">
+                    <img src="{{ asset('images/logo.png') }}" width="70px" alt="">
                 </a>
             </div>
 
             <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-            <a href="#">Dashboard</a>
-            <a href="#">My Profile</a>
-            <a href="#">Products</a>
-            <a href="#">Places</a>
-            <a href="#">Users</a>
-            <a href="#">History</a>
+            @foreach ($navbars as $navbarItem)
+            <li class="nav-item list-unstyled">
+                <a class="nav-link" href="{{ route($navbarItem->route) }}">{{ $navbarItem->name }}</a>
+            </li>
+            @endforeach
         </div>
     </div>
 
