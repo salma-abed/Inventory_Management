2d564bd68f2026c5e5e65c7d1095b2178990250e
Yosr Gamal <yosr.elqott@gmail.com>
2022-05-05 17:19:40 +0200

    renamed mainmenu

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -10,7 +10,7 @@
         <header class="row">
             @include('layouts.navbar')
         </header>
-        @include('layouts.mainmenu')
+        @include('layouts.mainmenuAdmin')
         <div id="main" class="row">
             @yield('content')
         </div>
