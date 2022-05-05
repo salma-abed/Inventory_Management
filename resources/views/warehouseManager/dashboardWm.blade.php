dd91f14dced701bbcc9b9c1fcaaa8ce379c182d2
Yosr Gamal <yosr.elqott@gmail.com>
2022-05-05 16:22:07 +0200

    dashboard for warehouse manager

---
+++ resources/views/warehouseManager/dashboardWm.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.default')
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Focus Admin: Creative Admin Dashboard</title>
+</head>
+
+<body>
+
+    <!-- /# sidebar -->
+
+
+
+    @section ('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        Dashboard
+                    </div>
+
+                    <div class="card-body">
+                        @if(session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                        @endif
+
+                        You are logged in!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endsection
+
+</body>
+
+</html>
