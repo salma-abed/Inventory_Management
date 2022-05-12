f6db2afcc1f58610cc007a4df65d2f449560d0fe
Dinaa19 <96875784+Dinaa19@users.noreply.github.com>
2022-05-12 22:01:14 +0200

    putting comment

--- resources/views/salespeople/storesTable.blade.php
+++ resources/views/salespeople/storesTable.blade.php
@@ -57,7 +57,7 @@
                                             </tr>
                                         </thead>
                                         <tbody>
-
+                                            ##
                                         </tbody>
 
                                     </table>
