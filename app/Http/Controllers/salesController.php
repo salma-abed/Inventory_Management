0102366506da069cc1c49953d4cba3ff3900406a
Yosr Gamal <yosr.elqott@gmail.com>
2022-05-29 22:31:12 +0200

    added sales controller

--- app/Http/Controllers/salesController.php
+++ app/Http/Controllers/salesController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Sales;
 
-class salesController extends Controller
+class SalesController extends Controller
 {
     public function view()
     {
