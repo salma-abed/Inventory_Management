592edc72f098d2a063685a08c20f7a7cad206a86
Dinaa19 <96875784+Dinaa19@users.noreply.github.com>
2022-06-01 01:49:15 +0200

    accessing and editing login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,15 +9,19 @@
                     <div class="card-body">
                         <form method="POST" action="{{ route('login.custom') }}">
                             @csrf
+                            @if (Session::has('error'))
+                            <span class="text-danger">{{ Session::get('error') }}</span>
+                            @endif
                             <div class="form-group mb-3">
-                                <input type="text" placeholder="Email" id="email" class="form-control" name="email" required
-                                    autofocus>
+                                <input type="text" placeholder="Email" id="email" class="form-control" name="email"
+                                    required autofocus>
                                 @if ($errors->has('email'))
                                 <span class="text-danger">{{ $errors->first('email') }}</span>
                                 @endif
                             </div>
                             <div class="form-group mb-3">
-                                <input type="password" placeholder="Password" id="password" class="form-control" name="password" required>
+                                <input type="password" placeholder="Password" id="password" class="form-control"
+                                    name="password" required>
                                 @if ($errors->has('password'))
                                 <span class="text-danger">{{ $errors->first('password') }}</span>
                                 @endif
