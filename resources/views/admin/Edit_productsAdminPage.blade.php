349928e795a47050ee8aee0bc2cf2904cc2e5777
Yosr Gamal <yosr.elqott@gmail.com>
2022-06-01 16:17:12 +0200

    added validation on edit forms

--- resources/views/admin/Edit_productsAdminPage.blade.php
+++ resources/views/admin/Edit_productsAdminPage.blade.php
@@ -3,7 +3,6 @@
 
 <body>
     @section('content')
-
     <div class="container-fluid">
         <div class="row justify-content-center">
             <div class="col-lg-6">
@@ -12,29 +11,44 @@
                     <form id="formElement" method="POST" action="/update1/{{$data[0]->product_id}}">
                         @csrf
                         <div class="mb-3">
-                            <label class="form-label">Place Name</label>
-                            <input type="text" class="form-control" id="name" value="{{$data[0]->name}}"
-                                name="name">
+                            <label class="form-label">Product Name</label>
+                            <input type="text" class="@error('name') is-invalid @enderror form-control" id="name"
+                                value="{{$data[0]->name}}" name="name">
+                            @error('name')
+                            <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                            @enderror
                         </div>
                         <div class="mb-3">
                             <label class="form-label">Location</label>
-                            <input type="text" class="form-control" id="location"
-                                value="{{$data[0]->location}}" name="location">
+                            <input type="text" class="@error('location') is-invalid @enderror form-control"
+                                id="location" value="{{$data[0]->location}}" name="location">
+                            @error('location')
+                            <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                            @enderror
                         </div>
                         <div class="mb-3">
                             <label class="form-label">description</label>
-                            <input type="text" class="form-control" id="description" value="{{$data[0]->description}}"
-                                name="description">
+                            <input type="text" class="@error('description') is-invalid @enderror form-control"
+                                id="description" value="{{$data[0]->description}}" name="description">
+                            @error('description')
+                            <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                            @enderror
                         </div>
                         <div class="mb-3">
                             <label class="form-label">Price</label>
-                            <input type="text" class="form-control" id="price" value="{{$data[0]->price}}"
-                                name="price">
+                            <input type="text" class="@error('price') is-invalid @enderror form-control" id="price"
+                                value="{{$data[0]->price}}" name="price">
+                            @error('price')
+                            <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                            @enderror
                         </div>
                         <div class="mb-3">
                             <label class="form-label">Quantity</label>
-                            <input type="text" class="form-control" id="quantity" value="{{$data[0]->quantity}}"
-                                name="quantity">
+                            <input type="text" class="@error('quantity') is-invalid @enderror form-control"
+                                id="quantity" value="{{$data[0]->quantity}}" name="quantity">
+                            @error('quantity')
+                            <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                            @enderror
                         </div>
                         <button type="submit" class="btn btn-primary" data-bs-dismiss="modal">Update</button>
                 </div>
