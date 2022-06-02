23593c61df622442387590941c0f874aefa21f26
Dinaa19 <96875784+Dinaa19@users.noreply.github.com>
2022-06-02 15:04:00 +0200

    adding redzone controller

--- app/Http/Controllers/RedZonesController.php
+++ app/Http/Controllers/RedZonesController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class RedZonesController extends Controller
 {
-    //
+    public function SendNotification(Request $request, $id)
+    {
+        $From_place = $request->input('From_place');
+        $Product = $request->input('product');
+        $quantity = DB::table('places')->select('quantity as AA')->where('place_name', $From_place)->where('product', $Product)->first();
+
+        $quantity = (int)$quantity->AA;
+        $PLUS = (int)$request->input('quantity');
+        $quantity = $quantity - $PLUS;
+        DB::update('update places set quantity=? where place_name=? ', [$quantity, $From_place]);
+        $range = 1000;
+
+        if ($quantity < $range) {
+
+            $String = "An amout of: " . $PLUS . " of Product: " . $Product . " is being out of stcok from " . $From_place;
+        }
+        DB::update('update places set quantity=? where place_name=? ', [$quantity, $From_place]);
+    }
 }
