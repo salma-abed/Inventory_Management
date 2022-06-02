354dfcf1d338d203f5b5d8266cdba3eeedc2228b
OmegaElrefaie <omegaelrefaie@gmail.com>
2022-06-02 08:51:55 +0200

    changed function name

--- app/Http/Controllers/WarehouseManagerController.php
+++ app/Http/Controllers/WarehouseManagerController.php
@@ -8,7 +8,7 @@ use App\Models\WarehouseManager;
 
 class WarehouseManagerController extends Controller
 {
-    public function index()
+    public function ViewPlaces()
     {
         //
         $data=DB::select("select * from places");
