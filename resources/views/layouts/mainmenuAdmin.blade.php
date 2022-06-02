fe81c9317ed5b125e6a9e6a473457cec84b6a783
Yosr Gamal <yosr.elqott@gmail.com>
2022-06-02 18:44:12 +0200

    routes of sidemenu

--- resources/views/layouts/mainmenuAdmin.blade.php
+++ resources/views/layouts/mainmenuAdmin.blade.php
@@ -8,11 +8,71 @@
             </div>
 
             <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-            @foreach ($navbars as $navbarItem)
+            @if(Auth::user()->user_type == 'admin')
             <li class="nav-item list-unstyled">
-                <a class="nav-link" href="{{ route($navbarItem->route) }}">{{ $navbarItem->name }}</a>
+                <a class="nav-link" href="dashboard">Dashboard</a>
             </li>
-            @endforeach
+            <li class="nav-item list-unstyled">
+                <a class="nav-link" href="profilePage">My Profile</a>
+            </li>
+            <li class="nav-item list-unstyled">
+                <a class="nav-link" href="productsAdmin">Products</a>
+            </li>
+            <li class="nav-item list-unstyled">
+                <a class="nav-link" href="placesAdmin">Places</a>
+            </li>
+            <li class="nav-item list-unstyled">
+                <a class="nav-link" href="users">Users</a>
+            </li>
+            <li class="nav-item list-unstyled">
+                <a class="nav-link" href="history">History</a>
+            </li>
+            <li class="nav-item list-unstyled">
+                <a class="nav-link" href="signout">Logout</a>
+            </li>
+            @elseif(Auth::user()->user_type == 'warehouse')
+            <li class="nav-item list-unstyled">
+                <a class="nav-link" href="dashboard">Dashboard</a>
+            </li>
+            <li class="nav-item list-unstyled">
+                <a class="nav-link" href="profilePage">My Profile</a>
+            </li>
+            <li class="nav-item list-unstyled">
+                <a class="nav-link" href="placesWm">Warehouses</a>
+            </li>
+            <li class="nav-item list-unstyled">
+                <a class="nav-link" href="signout">Logout</a>
+            </li>
+            @elseif(Auth::user()->user_type == 'operations')
+            <li class="nav-item list-unstyled">
+                <a class="nav-link" href="dashboard">Dashboard</a>
+            </li>
+            <li class="nav-item list-unstyled">
+                <a class="nav-link" href="profilePage">My Profile</a>
+            </li>
+            <li class="nav-item list-unstyled">
+                <a class="nav-link" href="productsOA">Products</a>
+            </li>
+            <li class="nav-item list-unstyled">
+                <a class="nav-link" href="placesOA">Places</a>
+            </li>
+            <li class="nav-item list-unstyled">
+                <a class="nav-link" href="signout">Logout</a>
+            </li>
+            @elseif(Auth::user()->user_type == 'sales')
+            <li class="nav-item list-unstyled">
+                <a class="nav-link" href="dashboard">Dashboard</a>
+            </li>
+            <li class="nav-item list-unstyled">
+                <a class="nav-link" href="profilePage">My Profile</a>
+            </li>
+            <li class="nav-item list-unstyled">
+                <a class="nav-link" href="stores">Stores</a>
+            </li>
+            <li class="nav-item list-unstyled">
+                <a class="nav-link" href="signout">Logout</a>
+            </li>
+            @endif
         </div>
     </div>
 
