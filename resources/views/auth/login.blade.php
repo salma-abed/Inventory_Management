047224243665d522c86b0874c2007d61a0563081
salma-abed <salma1910328@miuegypt.edu.eg>
2022-06-02 13:20:47 +0200

    front

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,18 @@
-@extends('layouts.default')
+@extends('app')
 @section('content')
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Login</title>
+
+    @include('layouts.links')
+</head>
+
 <main class="login-form">
     <div class="cotainer">
         <div class="row justify-content-center">
@@ -44,3 +57,4 @@
     </div>
 </main>
 @endsection
+</html>
