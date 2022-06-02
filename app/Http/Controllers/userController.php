ad46c1601344c1ef0cf692b3d3555f9600d84271
OmegaElrefaie <omegaelrefaie@gmail.com>
2022-06-02 08:43:29 +0200

    deleting view() default function

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -6,10 +6,6 @@ use Illuminate\Http\Request;
 
 class userController extends Controller
 {
-    public function view()
-    {
-        return view('users');
-    }
 
     public function index()
     {
