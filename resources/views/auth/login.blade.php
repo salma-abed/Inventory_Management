fdbfde6a4bff6cdcf414c727711ff82e7bcd004b
salma-abed <salma1910328@miuegypt.edu.eg>
2022-06-02 14:28:12 +0200

    vzv

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,17 +56,17 @@
                             <div class="d-grid mx-auto">
                                 <button type="submit" class="btn btn-dark btn-block">LOGIN</button>
                             </div>
-                             <div class="social-login-content">
-                                <div class="social-button">
+                                    <div class="social-login-content">
+                                             <div class="social-button">
                                     <button type="button"
                                         class="btn btn-primary bg-google btn-flat btn-addon m-t-80">
                                         <i class="fa-brands fa-google"></i>Sign in with google</button>
-                                </div>
-                            </div>
+                                            </div>
+                                    </div>
 
                         </form>
                     </div>
-                </div>
+                 </div>
                 </div>
             </div>
         </div>
