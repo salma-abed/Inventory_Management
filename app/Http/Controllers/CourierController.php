aecbcb7195230e893ec2fa465c3b73ca82c3ebe8
Yosr Gamal <yosr.elqott@gmail.com>
2022-06-03 12:21:19 +0200

    made changes

--- app/Http/Controllers/CourierController.php
+++ app/Http/Controllers/CourierController.php
@@ -12,74 +12,12 @@ class CourierController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function GetProductQuantity()
     {
-        //
-    }
+        $data =  DB::select("select quantity from products");
+        $arr['data'] = $data;
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+        return view('admin/productsAdminPage', $arr);
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
