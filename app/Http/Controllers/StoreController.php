1d4146f1be6c7061d42bc8800f123b6c9db181c6
Yosr Gamal <yosr.elqott@gmail.com>
2022-06-03 14:59:45 +0200

    changed return of view url

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -9,6 +9,7 @@ use App\Models\Store;
 
 class StoreController extends Controller
 {
+
     /**
      * Display a listing of the resource.
      *
@@ -19,14 +20,14 @@ class StoreController extends Controller
         $data =  DB::select("select quantity from products");
         $arr['data'] = $data;
 
-        return view('admin/productsAdminPage', $arr);
+        return view('salespeople/storesTable', $arr);
     }
     public function SoldProductQuantity()
     {
         $data =  DB::select("select quantity from products");
         $arr['data'] = $data;
 
-        return view('admin/productsAdminPage', $arr);
+        return view('salespeople/storesTable', $arr);
     }
 
     public function ViewProducts()
@@ -34,85 +35,21 @@ class StoreController extends Controller
         $data =  DB::select("select * from products");
         $arr['data'] = $data;
 
-        return view('admin/productsAdminPage', $arr);
+        return view('salespeople/storesTable', $arr);
     }
     public function GetLocations()
     {
         $data =  DB::select("select location from products");
         $arr['data'] = $data;
 
-        return view('admin/productsAdminPage', $arr);
+        return view('salespeople/storesTable', $arr);
     }
     public function GetProductQuantity()
     {
         $data =  DB::select("select quantity from products");
         $arr['data'] = $data;
 
-        return view('admin/productsAdminPage', $arr);
+        return view('salespeople/storesTable', $arr);
     }
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+  
 }
