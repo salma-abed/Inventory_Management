121e4243f0ca940183ded163923362625507f741
OmegaElrefaie <omegaelrefaie@gmail.com>
2022-06-04 03:35:02 +0200

    editing the form of editing quantity SalesPerson

--- resources/views/admin/Edit_Quantity.blade.php
+++ resources/views/admin/Edit_Quantity.blade.php
@@ -8,20 +8,20 @@
         <div class="row justify-content-center">
             <div class="col-lg-6">
                 <div class=" login-form">
-                    <h3>Edit</h3>
+                    <h3>Selling</h3>
                     <form id="formElement" method="POST" action="/updateQuantity/{{$data[0]->place_id}}">
                         @csrf
                         
                        
                        
                         <div class="mb-3">
-                            <label class="form-label">Quantity</label>
-                            <input type="text" class="@error('quantity') is-invalid @enderror form-control"
-                                id="quantity" value="{{$data[0]->quantity}}" name="quantity">
+                            <label class="form-label">enter Quantity to sell</label>
+                            <input type="text" class="@error('quantity') is-invalid @enderror form-control" id="quantity"  name="quantity">
                             @error('quantity')
                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                             @enderror
                         </div>
+                        
                         
                         <button type="submit" class="btn btn-primary" data-bs-dismiss="modal">Update</button>
                 </div>
