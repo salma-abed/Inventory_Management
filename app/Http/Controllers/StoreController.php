c2f214fa0bb038ad85dfb001f5c51f312dab3e53
OmegaElrefaie <omegaelrefaie@gmail.com>
2022-06-04 03:12:47 +0200

    added editing function in StoresController

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\products;
 use App\Models\Store;
+use App\Models\places;
 
 class StoreController extends Controller
 {
@@ -28,6 +29,36 @@ class StoreController extends Controller
         $arr['data'] = $data;
 
         return view('salespeople/storesTable', $arr);
+    }
+    
+    
+    public function EditQuantity($places_id)
+    {
+        $data=  DB::select('select * from places where place_id=?',[$places_id]);
+        return view('admin/Edit_Quantity',['data'=>$data]);
+
+
+    }
+    
+
+    public function updateQuantity(Request $request, $id)
+    {
+
+
+        $data=  DB::select('select * from places where place_id=?',[$id]);
+
+        $quantityDB=$data[0]->quantity;
+
+        $quantityUser= $request->input('quantity');
+
+        $newQuantity=(int)$quantityDB-(int)$quantityUser;
+
+
+        DB::update('update places set quantity=? where place_id=? ',[$newQuantity,$id]);
+
+
+        return redirect('Stores');    //redirects sends to the page specified
+        
     }
 
     public function ViewProducts()
